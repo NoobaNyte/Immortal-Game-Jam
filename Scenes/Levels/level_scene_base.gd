5188d1e38c80b09b4e97ae64433d1a52a00cf61e
NoobaNyte <lukecopa123@gmail.com>
2026-09-19 20:43:23 -0600

    added level entrances and exits

--- Scenes/Levels/level_scene_base.gd
+++ Scenes/Levels/level_scene_base.gd
@@ -2,6 +2,27 @@ extends Node2D
 class_name LevelSceneBase
 
 @onready var player_character: CharacterBody2D = find_child("PlayerCharacter")
+@onready var enter_level_area: Area2D = find_child("EnterLevelArea")
+@onready var exit_level_area: Area2D = find_child("ExitLevelArea")
+
+@export_file_path var next_level_scene_file_path
+@export_file_path var previous_level_scene_file_path
 
 func _ready() -> void:
 	player_character.show()
+	enter_level_area.body_entered.connect(_on_enter_level_area_entered)
+	exit_level_area.body_entered.connect(_on_exit_level_area_entered)
+
+func _on_enter_level_area_entered(body: Node2D):
+	if body.name == "PlayerCharacter":
+		if previous_level_scene_file_path:
+			Global.load_next_level.emit(previous_level_scene_file_path)
+		else:
+			push_warning("cannot transition levels! previous_level_scene_file_path not assigned in inspector")
+
+func _on_exit_level_area_entered(body: Node2D):
+	if body.name == "PlayerCharacter":
+		if next_level_scene_file_path:
+			Global.load_next_level.emit(next_level_scene_file_path)
+		else:
+			push_warning("cannot transition levels! next_level_scene_file_path not assigned in inspector")
