8a9f2c0d283c6af2a27f6cb2c4a4325fd2edb735
NoobaNyte <lukecopa123@gmail.com>
2026-09-20 14:23:54 -0600

    updating boot screen

--- Scenes/LoadingScreens/LoadingScreen/loading_screen.gd
+++ Scenes/LoadingScreens/LoadingScreen/loading_screen.gd
@@ -4,10 +4,6 @@ extends BaseUIElement
 
 @export var rotation_speed: float = 2.5 ## Speed of the clockwise rotation in radians per second
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Multiplying by delta ensures smooth, frame-rate independent rotation on web and low-end devices
